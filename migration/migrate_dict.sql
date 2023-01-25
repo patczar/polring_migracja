e111d0a79e9e86745564fe4caf456def7377d1d5
Patryk Czarnik <patryk@patrykczarnik.net>
2023-01-25 11:45:44 +0100

    Lepszy zapis dla uzupełnienia gatunków.

--- migration/migrate_dict.sql
+++ migration/migrate_dict.sql
@@ -1,4 +1,6 @@
 -- Migracja tabel słownikowych
+
+-- Tymczasowo: kopiowanie brakujących rekordów do dict_species (główna baza zmieniłą się względem starego zrzutu).
 INSERT INTO public.dict_species(id, eu_code, abbr, text_pl, text_en, text_la, is_active, is_default, sort_order, created_at, updated_at)
 SELECT id_gat,
        kodeu,
@@ -12,5 +14,7 @@ SELECT id_gat,
        current_timestamp,
        current_timestamp
 FROM dbo.ptaki_bk_gatunek
-WHERE id_gat >= 1511;
+WHERE
+-- id_gat >= 1511
+id_gat NOT IN (SELECT id FROM public.dict_species)
 ;
